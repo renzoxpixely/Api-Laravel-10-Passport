e64f48ea61524144f699ac255ba3b55ce2059295
renzoxpixely <renzo_or@hotmail.com>
2023-06-30 04:15:58 -0500

    CRUD Blog complete

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\BlogResource;
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Exception;
+use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
@@ -14,27 +17,32 @@ class BlogController extends Controller
      */
     public function index()
     {
-        //
-        $blogs = Blog::all();
+        //$blogs = Blog::all();
+        $blogs = Blog::orderBy('id','desc')->get();
         return sendResponse('Success', BlogResource::collection($blogs));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required|min:10|max:255',
+            'description'    => 'required|min:20'
+        ]);
 
+        if ($validator->fails()) return sendError('Validation Error.', $validator->errors(), 422);
+
+        try {
+            $blog = Blog::create([
+                'title' => $request->title,
+                'description' => $request->description
+            ]);
+            return sendResponse('Blog create success!', new BlogResource($blog));
+        } catch (Exception $e) {
+            return sendError($e->getMessage(), $e->getCode());
+        }
+    }
     /**
      * Display the specified resource.
      */
@@ -43,32 +51,44 @@ class BlogController extends Controller
         $blog = Blog::find($id);
 
         if($blog)
-            return sendResponse('Success', $blog);
+            return sendResponse('Success', new BlogResource($blog));
         else
             return sendError('Data not found');
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Blog $blog)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Blog $blog)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required|min:10|max:255',
+            'description'    => 'required|min:20'
+        ]);
 
+        if ($validator->fails()) return sendError('Validation Error.', $validator->errors(), 422);
+
+        try {
+            $blog->title = $request->title;
+            $blog->description = $request->description;
+            $blog->save();
+            return sendResponse('Blog update success!', new BlogResource($blog));
+        } catch (Exception $e) {
+            return sendError($e->getMessage(), $e->getCode());
+        }
+    }
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Blog $blog)
+    public function destroy($id)
     {
-        //
+        try {
+            $blog = Blog::find($id);
+            if ($blog){
+                $blog->delete();
+            }
+            return sendError('Blog deleted success!', []);
+        } catch (Exception $e) {
+            return sendError('Something wrong!', $e->getCode());
+        }        
     }
 }
